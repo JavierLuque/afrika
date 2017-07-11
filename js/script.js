47386d664f03da243ee459c90572438a907c6a16
dpopovici1 <dimapopovici@gmail.com>
2017-07-11 14:59:31 -0400

    tour-page-summary

--- js/script.js
+++ js/script.js
@@ -624,6 +624,50 @@ $(document).ready(function(){
 
     
     //------------------------------------------------------------------------//
+  //################################################
+  //--m-select
+  //################################################
+
+  (function () {
+
+    var classname_m_select_btn = $(".m-select-btn");
+
+    if (classname_m_select_btn.length != 0) {
+
+      var dropdown_links = $(".m-select-dropdown a");
+
+
+      var openDropdown = function(evt) {
+          evt.preventDefault();
+          this.classList.toggle('open-dropdown');
+      };
+      var selectOption = function(evt) {
+          evt.preventDefault();
+          var data_name = this.getAttribute("data-name");
+          var data_value = this.getAttribute("data-value");
+          var data_parent = this.getAttribute("data-parent");
+          var inp = document.querySelector('#' + data_parent + '-value');
+          var btn = document.querySelector('#' + data_parent + '-name');
+          var parent_link = document.querySelector('#' + data_parent + " .m-select-btn");
+
+          inp.value = data_value;
+          btn.innerHTML = data_name;
+          parent_link.classList.remove('open-dropdown');
+          // this.classList.toggle('open-dropdown');
+      };
+
+      for (var i = 0; i < classname_m_select_btn.length; i++) {
+        console.log('find one');
+          classname_m_select_btn[i].addEventListener('click', openDropdown, false);
+      }
+
+      for (var i = 0; i < dropdown_links.length; i++) {
+        console.log('find a link');
+          dropdown_links[i].addEventListener('click', selectOption, false);
+      }
+    
+    }
+  })();
 
     //Popup country
     // function deselect(e) {
